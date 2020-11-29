80f04e782c7f86c8d8b47014e164210dab71974e
kv322 <kv322@njit.edu>
2020-11-29 15:39:42 -0500

    Initial commit

---
+++ src/main/java/com/njit/cs631/dbms/Application.java
@@ -0,0 +1,13 @@
+package com.njit.cs631.dbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/njit/cs631/dbms/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njit.cs631.dbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
